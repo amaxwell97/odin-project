c960d12e6d7c732c3027d4f71746437709a38b3d
Avery Maxwell <averymaxwell42@gmail.com>
2025-01-12 16:04:07 -0600

    Set vars for html containers and declare functions

---
+++ odin-library-app/app.js
@@ -0,0 +1,13 @@
+const displayContainer = document.querySelector('display-container');
+
+const bookWrapper = document.querySelector('book-wrapper');
+
+const myLibrary = [];
+
+function Book() {
+
+};
+
+function addBookToLibrary () {
+
+};
