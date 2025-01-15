8563c0cd9747984c3acff8821e5b96c1eaaa2da8
Avery Maxwell <averymaxwell42@gmail.com>
2025-01-14 19:05:37 -0600

    Create variable that stores an array - Add three book objects to the array
    Update Book constructor function
    - Add three arguments
    - Set new objects to store arguments as properties
    
    Update addBookToLibrary function
    - Add three arguements
    - Declare variable newBook in the function that
    creates and stores a new Book oject
    - Push newBook to myLibrary array
    
    Create the function displayBooksInLibrary
    - Declare variable displayLibrary in the function
    - Store a new array in displayLibrary that
    contains the html for each book in myLibrary
    - Join the displayLibrary array and set it the
    innerHTML of displayContainer
    
    Add a load event listener to the window
    - Call displayBooksInLibrary() when the browser
    window loads

--- odin-library-app/app.js
+++ odin-library-app/app.js
@@ -1,13 +1,73 @@
-const displayContainer = document.querySelector('display-container');
+const displayContainer = document.querySelector('.display-container');
 
-const bookWrapper = document.querySelector('book-wrapper');
+const bookWrapper = document.querySelector('.book-wrapper');
 
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: 'The Obstacle is the Way',
+        author: 'Ryan Holiday',
+        numPages: 162,
+    },
+    {
+        title: 'Ego is the Enemy',
+        author: 'Ryan Holiday',
+        numPages: 211,
+    },
+    {
+        title: 'Think Again',
+        author: 'Adam Grant',
+        numPages: 324,
+    },
+];
 
-function Book() {
-
+function Book (title, author, numPages) {
+    // this.img = img,
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    // this.readStatus = ,
 };
 
-function addBookToLibrary () {
-
+function addBookToLibrary (title, author, numPages) {
+    let newBook = new Book(title, author, numPages);
+    console.log(newBook);
+    myLibrary.push(newBook);
 };
+
+function displayBooksInLibrary () {
+    // let library = myLibrary;
+    let displayLibrary = myLibrary.map((ev) => {
+        return `<div class="book-wrapper" name="book-1">
+                <div class="book-details">
+                    <p class="title">${ev.title}</p>
+                    <p class="author">${ev.author}</p>
+                    <p class="number-pages">${ev.numPages}</p>
+                    <label for="read-status">Read?</label>
+                    <input type="checkbox" name="read-status" id="">
+                </div>
+            </div>`;
+    });
+    console.log(displayLibrary);
+    displayLibrary.join('');
+    console.log(displayLibrary);
+    displayContainer.innerHTML = displayLibrary;
+    // for (let i = 0; i < library.length; i++) {
+    //     let bookEntry = `<div class="book-wrapper" name="book-${i + 1}">
+    //             <img src="/images/stillness-is-the-key.jpg" alt="">
+    //             <div class="book-details">
+    //                 <p class="title">${library[i].title}</p>
+    //                 <p class="author">${library[i].author}</p>
+    //                 <p class="number-pages">${library[i].numPages}</p>
+    //                 <label for="read-status">Read?</label>
+    //                 <input type="checkbox" name="read-status" id="">
+    //             </div>
+    //         </div>`;
+    //     console.log(bookEntry);
+    //     displayContainer.append(bookEntry);
+    // }
+};
+
+window.addEventListener('load', () => {
+    console.log(myLibrary)
+    displayBooksInLibrary();
+})
